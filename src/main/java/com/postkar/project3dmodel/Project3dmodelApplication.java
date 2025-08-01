02d1c8dea96284492f792d2e6581c612142770c2
Arpan Gautam <arpangautam2702@gmail.com>
2025-08-01 14:21:52 +0530

    First commit

---
+++ src/main/java/com/postkar/project3dmodel/Project3dmodelApplication.java
@@ -0,0 +1,13 @@
+package com.postkar.project3dmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3dmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3dmodelApplication.class, args);
+	}
+
+}
