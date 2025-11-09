f7945d93bc65fcb64cda64e0534832a6ea63d44f
Arpan Gautam <arpangautam2702@gmail.com>
2025-11-09 19:54:19 +0530

    0.3-updates-Changes for fast loading

---
+++ src/main/resources/static/assets/js/useNavigation-ZtrtozXF.js
@@ -0,0 +1,2 @@
+import{b as a}from"./index-D2QhjBB8.js";const t=()=>a();export{t as u};
+//# sourceMappingURL=useNavigation-ZtrtozXF.js.map
--- src/main/resources/static/assets/js/useNavigation-zugMbzF2.js
+++
@@ -1,2 +0,0 @@
-import{b as a}from"./index-DQxPBWdd.js";const t=()=>a();export{t as u};
-//# sourceMappingURL=useNavigation-zugMbzF2.js.map
